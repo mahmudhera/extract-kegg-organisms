a69684336c77804ada82ecb8a795e3ff6a204ccd
Mahmudur <mahmudhera93@gmail.com>
2022-11-02 13:28:34 -0400

    added different abundance estimates

--- src/summarize_ko_level_info.py
+++ src/summarize_ko_level_info.py
@@ -22,6 +22,8 @@ if __name__ == '__main__':
     nt_ovelaps = gene_abundance_df['nucleotide_overlap'].tolist()
     list_reads_mapped = gene_abundance_df['reads_mapped'].tolist()
     num_bases_of_the_gene_covered = gene_abundance_df['num_nts_in_reads'].tolist()
+    mean_coverages = gene_abundance_df['mean_coverage'].tolist()
+    median_coverages = gene_abundance_df['median_coverage'].tolist()
 
     gene_koid_df = pd.read_csv(present_genes_filename)
     gene_ids = gene_koid_df['gene_id'].tolist()
@@ -34,23 +36,36 @@ if __name__ == '__main__':
     total_num_reads = sum(list_reads_mapped)
     total_nucleotides_covered = sum(nt_ovelaps)
     total_bases_of_the_gene_covered_by_reads = sum(num_bases_of_the_gene_covered)
+    sum_mean_coverage = sum(mean_coverages)
+    sum_median_coverag = sum(median_coverages)
 
-    ko_abundances = {}
+    ko_abundances_by_num_reads = {}
+    ko_abundances_by_num_nts_in_reads = {}
+    ko_abundances_by_mean_cov = {}
+    ko_abundances_by_med_cov = {}
 
-    for gene_id, gene_length, nt_overlap, num_reads_mapped, num_bases_covered in list( zip(gene_names, gene_lengths, nt_ovelaps, list_reads_mapped, num_bases_of_the_gene_covered) ):
+    for gene_id, gene_length, nt_overlap, num_reads_mapped, num_bases_covered, mean_cov, med_cov in list( zip(gene_names, gene_lengths, nt_ovelaps, list_reads_mapped, num_bases_of_the_gene_covered, mean_coverages, median_coverages) ):
         abundance_estimate_1 = 1.0 * num_reads_mapped / total_num_reads
         abundance_estimate_2 = 1.0 * nt_overlap / total_nucleotides_covered
         abundance_estimate_3 = 1.0 * num_bases_covered / total_bases_of_the_gene_covered_by_reads
+        abundance_estimate_4 = 1.0 * mean_cov / sum_mean_coverage
+        abundance_estimate_5 = 1.0 * med_cov / sum_median_coverag
 
         ko_id = gene_id_to_ko_id[gene_id]
-        if ko_id in ko_abundances.keys():
-            ko_abundances[ko_id] += abundance_estimate_3
+        if ko_id in ko_abundances_by_num_reads.keys():
+            ko_abundances_by_num_reads[ko_id] += abundance_estimate_1
+            ko_abundances_by_num_nts_in_reads[ko_id] += abundance_estimate_3
+            ko_abundances_by_mean_cov[ko_id] += abundance_estimate_4
+            ko_abundances_by_med_cov[ko_id] += abundance_estimate_5
         else:
-            ko_abundances[ko_id] = abundance_estimate_3
+            ko_abundances_by_num_reads[ko_id] = abundance_estimate_1
+            ko_abundances_by_num_nts_in_reads[ko_id] = abundance_estimate_3
+            ko_abundances_by_mean_cov[ko_id] = abundance_estimate_4
+            ko_abundances_by_med_cov[ko_id] = abundance_estimate_5
 
     out_list = []
     for ko_id in ko_abundances.keys():
-        out_list.append((ko_id, ko_abundances[ko_id]))
+        out_list.append((ko_id, ko_abundances_by_num_reads[ko_id], ko_abundances_by_num_nts_in_reads[ko_id], ko_abundances_by_mean_cov[ko_id], ko_abundances_by_med_cov[ko_id]))
 
-    out_df = pd.DataFrame(out_list, columns=['ko_id', 'abundance'])
+    out_df = pd.DataFrame(out_list, columns=['ko_id', 'abund_by_num_reads', 'abund_by_nts_covered_in_reads', 'abund_by_mean_cov', 'abund_by_median_cov'])
     out_df.to_csv( output_filename )
