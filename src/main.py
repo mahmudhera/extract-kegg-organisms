c388c02d3223cd9c09d12c5a3fbac54105e64ec2
Mahmudur <mahmudhera93@gmail.com>
2023-06-02 09:34:11 -0400

    updated dir names for new setup

--- src/main.py
+++ src/main.py
@@ -8,85 +8,6 @@ import subprocess
 from tqdm import tqdm
 import sys
 
-def read_organism_table(org_table_filename):
-    """
-    Takes as argument the organism table filename with full path.
-    Returns a dictionary indexed by 3-character organism code, mapping to a list
-    of contig ids.
-    :param str org_table_filename: full path to organism filename
-    :return: dictionary indexed by 3-character organism code, mapping to a list
-    of contig ids.
-    :rtype: dic
-    """
-    # read dataframe
-    df = pd.read_csv(org_table_filename, delimiter='\t')
-
-    # keep only bacteria
-    df['lineage'] = df['lineage'].str.lower()
-    df = df[ df['lineage'].str.contains('bacteria') ]
-
-    # take interesting things
-    gb_ids = df['gb_ncbi_seq_id'].tolist()
-    org_codes = df['org_code'].tolist()
-    names = df['name'].tolist()
-
-    # keep only non-empty ncbi things here
-    ret_dic = {}
-    for gb_id, org_code, name in list( zip(gb_ids, org_codes, names) ):
-        if gb_id == '[]':
-            continue
-        else:
-            gb_id = gb_id[1:-1].replace('\'', '').split(',')
-            ret_dic[org_code] = gb_id
-
-    return ret_dic
-
-def read_organism_table_for_single_chr_bacteria(org_table_filename):
-    """
-    Takes as argument the organism table filename with full path.
-    Returns a dictionary indexed by 3-character organism code, mapping to a list
-    of contig ids. Only those bacteria with single chromosome are kept.
-    :param str org_table_filename: full path to organism filename
-    :return: dictionary indexed by 3-character organism code, mapping to a list
-    of contig ids.
-    :rtype: dic
-    """
-    # read dataframe
-    df = pd.read_csv(org_table_filename, delimiter='\t')
-
-    # keep only bacteria
-    df['lineage'] = df['lineage'].str.lower()
-    df = df[ df['lineage'].str.contains('bacteria') ]
-
-    # take interesting things
-    gb_ids = df['gb_ncbi_seq_id'].tolist()
-    org_codes = df['org_code'].tolist()
-    names = df['name'].tolist()
-
-    # keep only non-empty ncbi things here
-    ret_dic = {}
-    for gb_id, org_code, name in list( zip(gb_ids, org_codes, names) ):
-        if gb_id == '[]':
-            continue
-        else:
-            gb_id = gb_id[1:-1].replace('\'', '').split(',')
-            if len(gb_id) > 1:
-                continue
-            ret_dic[org_code] = gb_id
-
-    return ret_dic
-
-def read_gene_ids(kegg_gene_filename):
-    """
-    Given a kegg gene filename, returns list of all genes
-    :param str kegg_gene_filename: filename with full path
-    :return: all gene ids in that file
-    :rtype: list
-    """
-    f = open(kegg_gene_filename, 'r')
-    lines = f.readlines()[1:]
-    f.close()
-    return [ line.split(' ')[0].split('\t')[0] for line in lines ]
 
 def read_gene_id_with_ko(kegg_gene_filename):
     """
@@ -128,8 +49,8 @@ def main(): # pragma: no cover
     args = parse_args()
     print('Test!')
 
-    genome_path = '/home/grads/mbr5797/kegg-extracted-data/extracted_genomes'
-    directory_with_kegg_gene_files = '/data/shared_data/KEGG_data/organisms/kegg_gene_info'
+    genome_path = '/scratch/mbr5797/genomes_extracted_from_kegg'
+    directory_with_kegg_gene_files = '/scratch/shared_data/archive/KEGG_data/organisms/kegg_gene_info'
 
     genome_names = []
     present_genes_and_kos = []
